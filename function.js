7cebbf75795584e23855b8f085fa7cbd0a7b45b5
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-22 09:39:50 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+function panggilEksternal(){
+	var firstname = document.getElementById('firstname')
+	var lastname = document.getElementById('lastname')
+	var hobi = document.getElementById('hobi')
+
+	console.log(firstname)
+	console.log(lastname)
+	console.log(hobi)
+}
